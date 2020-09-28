81cbe4936c8e19474c948511b8e484e1a157e265
unknown <tanwesley88@gmail.com>
2020-09-28 11:43:08 -0500

    .

---
+++ src/mazegame/MazeApp.java
@@ -0,0 +1,13 @@
+package mazegame;
+import java.util.Scanner;
+
+public class MazeApp {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter name of file: ");
+		String fname = sc.nextLine();
+		Building bldg = BuildingBuilder.buildFromFile(fname);
+		GameManager gm = new GameManager(bldg);
+		gm.play(bldg);
+	}
+}
